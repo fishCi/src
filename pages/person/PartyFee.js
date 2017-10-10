a2137ca2df9c2fd3cc9bc4a343b61a5fee6ad24c
zhaozheng <zzelune@yeah.net>
2017-10-10 17:11:10 +0800

    10101711

--- pages/person/PartyFee.js
+++ pages/person/PartyFee.js
@@ -2,7 +2,7 @@
 * @Author: miaoxinyu.zh
 * @Date:   2017-08-22 06:06:10
  * @Last Modified by: zhaozheng1.zh
- * @Last Modified time: 2017-09-25 09:30:28
+ * @Last Modified time: 2017-10-10 17:07:15
 */
 import React from 'react';
 import {
@@ -17,317 +17,112 @@ import {
 import { Button, Card, CardItem } from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import EmptyView from '../../components/EmptyView';
-
+import { fetchPost } from '../../utils/fetchAPI';
 
 class PartyFee extends React.Component {
 
-  constructor(props) {
+  constructor(propretuers) {
     super(props);
-
     this.state = {
-      data: {
-        flatListData: [{
-          key: '2017年1月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2017年2月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2017年3月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2017年4月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2017年5月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2017年6月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2017年7月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2017年8月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2017年9月',
-          text: '143',
-          col: '2'
-        },
-        {
-          key: '2017年10月',
-          text: '',
-          col: '3'
-        },
-        {
-          key: '2017年11月',
-          text: '',
-          col: '3'
-        },
-        {
-          key: '2017年12月',
-          text: '',
-          col: '3'
-        }],
-      }
+      ready: false,
+      year: '2017',
+    }
+  }
+  data = []
 
-    };
+  componentDidMount() {
+    fetchPost('A08463104', {
+      Pty_Grp_Stm_Usr_ID: '01000364',
+      Yr_YYYY: '2017'
+    }, this._success.bind(this), this._failure.bind(this))
   }
 
+
+
+  _success(resp) {
+    if (resp.BK_STATUS == "00") {
+      for (let i = 0; i < resp.LIST1.length; i++) {
+        let item = {
+          month = '',
+          money = '',
+          status = '00'
+        };
+        item.month = resp.LIST1[i].Mo_MO;
+        item.money = resp.LIST1[i].Pbl_Pty_Fee_Amt;
+        item.status = resp.LIST1[i].Py_StCd;
+        this.data.push(item);
+      }
+      this.setState({ ready: true })
+    } else {
+      alert(resp.BK_DESC)
+    }
+  };
+
+  _failure(error) {
+    alert(error);
+  };
+
+  _onClick = year => {
+    fetchPost('A08463104', {
+      Pty_Grp_Stm_Usr_ID: '01000364',
+      Yr_YYYY: year
+    },
+      (resp) => {
+        if (resp.BK_STATUS == "00" && resp.ScsInd == "0") {
+          this.data =[];
+          for (let i = 0; i < resp.LIST1.length; i++) {
+            let item = {
+              month = '',
+              money = '',
+              status = '00'
+            };
+            item.month = resp.LIST1[i].Mo_MO;
+            item.money = resp.LIST1[i].Pbl_Pty_Fee_Amt;
+            item.status = resp.LIST1[i].Py_StCd;
+            this.data.push(item);
+          }
+        } else {
+          alert(resp.BK_DESC)
+        }
+      },
+      (error) => {
+        alert(error)
+      }
+    )
+    this.setState({ year: year});
+  }
+
+
   _renderItemComponent = ({ item }) => {
-    let flag = '';
-    switch (item.col) {
-      case '1':
-        flag = 'skyblue';
+    let color = '';
+    switch (item.status) {
+      case '02':
+        color = 'skyblue';
         break;
-      case '2':
-        flag = 'red';
+      case '01':
+        color = 'red';
         break;
       default:
-        flag = 'grey';
+        color = 'grey';
     }
     return (
       <View style={{ flexDirection: 'row', width: 115 }}>
         <Card>
-          <CardItem style={{ backgroundColor: flag, justifyContent: 'center' }}>
-            <Text>{item.key}</Text>
+          <CardItem style={{ backgroundColor: color, justifyContent: 'center' }}>
+            <Text>{item.month}</Text>
           </CardItem>
           <CardItem style={{ justifyContent: 'center' }}>
-            <Text>{item.text}</Text>
+            <Text>{item.money}</Text>
           </CardItem>
         </Card>
       </View>
     );
+  }
 
-  }
-  onShowNow() {
-    //console.log(JSON.stringify(this.state.data))
-    // console.log()
-    var thiz = this;
-    thiz.setState({
-      data: {
-        flatListData: [{
-          key: '2017年1月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2017年2月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2017年3月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2017年4月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2017年5月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2017年6月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2017年7月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2017年8月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2017年9月',
-          text: '143',
-          col: '2'
-        },
-        {
-          key: '2017年10月',
-          text: '',
-          col: '3'
-        },
-        {
-          key: '2017年11月',
-          text: '',
-          col: '3'
-        },
-        {
-          key: '2017年12月',
-          text: '',
-          col: '3'
-        }],
-      }
-    });
-  }
-  onShowPast1() {
-    //console.log(JSON.stringify(this.state.data))
-    // console.log()
-    var thiz = this;
-    thiz.setState({
-      data: {
-        flatListData: [{
-          key: '2016年1月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2016年2月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2016年3月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2016年4月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2016年5月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2016年6月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2016年7月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2016年8月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2016年9月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2016年10月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2016年11月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2016年12月',
-          text: '143',
-          col: '1'
-        }],
-      }
-    });
-  }
-  onShowPast2() {
-    //console.log(JSON.stringify(this.state.data))
-    // console.log()
-    var thiz = this;
-    thiz.setState({
-      data: {
-        flatListData: [{
-          key: '2015年1月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2015年2月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2015年3月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2015年4月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2015年5月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2015年6月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2015年7月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2015年8月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2015年9月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2015年10月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2015年11月',
-          text: '143',
-          col: '1'
-        },
-        {
-          key: '2015年12月',
-          text: '143',
-          col: '1'
-        }],
-      }
-    });
-  }
+
+
   render() {
-    //alert(JSON.stringify(this.state.data));
     return (
-
       <View style={{ flex: 1, margin: 10 }}>
         <ScrollView>
           <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
@@ -337,7 +132,7 @@ class PartyFee extends React.Component {
               <Text>  143</Text>
             </View>
           </View>
-          <EmptyView h={10}/>
+          <EmptyView h={10} />
           <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
             <Text style={{ flex: 1 }}>历史已缴党费：</Text>
             <View style={{ flexDirection: 'row' }}>
@@ -345,16 +140,16 @@ class PartyFee extends React.Component {
               <Text>  10000</Text>
             </View>
           </View>
-          <EmptyView h={10}/>
+          <EmptyView h={10} />
           <View style={{ height: 1, backgroundColor: 'grey' }} />
           <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 5 }}>
-            <Button bordered style={{ justifyContent: 'center', height: 20 }} onPress={this.onShowNow.bind(this)}><Text>2017年</Text></Button>
-            <Button bordered style={{ justifyContent: 'center', height: 20 }} onPress={this.onShowPast1.bind(this)}><Text>2016年</Text></Button>
-            <Button bordered style={{ justifyContent: 'center', height: 20 }} onPress={this.onShowPast2.bind(this)}><Text>2015年</Text></Button>
+            <Button bordered style={{ justifyContent: 'center', height: 20 }} onPress={() => this._onClick('2017')}><Text>2017年</Text></Button>
+            <Button bordered style={{ justifyContent: 'center', height: 20 }} onPress={() => this._onClick('2016')}><Text>2016年</Text></Button>
+            <Button bordered style={{ justifyContent: 'center', height: 20 }} onPress={() => this._onClick('2015')}><Text>2015年</Text></Button>
           </View>
-          <EmptyView h={20}/>
+          <EmptyView h={20} />
           <FlatList
-            data={this.state.data.flatListData}
+            data={this.data}
             horizontal={false}
             numColumns={3}
             columnWrapperStyle={{ justifyContent: 'space-around' }}
